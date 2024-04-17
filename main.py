1fb6ab6fdda6c78d760bc54f3b8e329d1fbf33ce
Byeongkuk <lunamundus1007@gmail.com>
2024-04-18 00:48:34 +0900

    #4.5 For Loops

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from random import randint
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
+    "tiktok.com"
+)
 
-print("Welcome to Python Casino!")
-
-pc_choice = randint(1, 100)
-
-playing = True
-
-while playing:
-    user_choice = int(input("Choose number (1-100): "))
-
-    if user_choice == pc_choice:
-        print(f"You win!")
-        playing = False
-    elif user_choice > pc_choice:
-        print(f"Lower than your choice!")
-    elif user_choice < pc_choice:
-        print(f"Higher than your choice!")
+for website in websites:
+    print(f"This website is {website}")
