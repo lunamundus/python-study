d8ebef63340bc81948ddb047824d7beb6187e365
Byeongkuk <lunamundus1007@gmail.com>
2024-04-22 00:52:10 +0900

    #6.3 Jobs

--- main.py
+++ main.py
@@ -11,4 +11,23 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Get data from URL finding job section
-jobs = soup.find("section", class_="jobs").find_all("li")
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+for job in jobs:
+    try:
+        title = job.find("span", class_="title").text
+    except:
+        title = ""
+    try:
+        region = job.find("span", class_="region").text
+    except:
+        region = ""
+    try:
+        company, position, _ = job.find_all("span", class_="company")
+        company = company.text
+        position = position.text
+    except:
+        company = ""
+        position = ""
+        
+    print(title, '-----', region, '-----', company)
