021cd8c3f9d26b65f8ada51463ea5578dd2cfe04
Byeongkuk <lunamundus1007@gmail.com>
2024-04-22 01:32:01 +0900

    #6.4 Recap

--- main.py
+++ main.py
@@ -13,21 +13,31 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Get data from URL finding job section
 jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
+all_jobs = []
+
 for job in jobs:
-    try:
-        title = job.find("span", class_="title").text
-    except:
-        title = ""
-    try:
-        region = job.find("span", class_="region").text
-    except:
-        region = ""
-    try:
-        company, position, _ = job.find_all("span", class_="company")
-        company = company.text
-        position = position.text
-    except:
-        company = ""
-        position = ""
-        
-    print(title, '-----', region, '-----', company)
+    title = job.find("span", class_="title").text
+
+    region = job.find("span", class_="region")
+    if region:
+        region = region.text
+
+    companies = job.find_all("span", class_="company")
+    company = companies[0].text
+    position = companies[1].text
+
+    url = job.find("div", class_="tooltip--flag-logo").next_sibling
+    if url:
+        url = url["href"]           # html 태그에서 attribute를 추출하는 방법
+
+    job_data = {
+        "title": title,
+        "company": company,
+        "position": position,
+        "region": region,
+        "URL": f"https://weworkremotely.com{url}"
+    }
+
+    all_jobs.append(job_data)
+
+print(all_jobs)
