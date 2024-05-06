19790464eacd50188eabec9818b8b4bdb5b07fc4
Byeongkuk <lunamundus1007@gmail.com>
2024-05-07 01:17:32 +0900

    #6.10 Interactivity

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 # playwright 실행
@@ -10,7 +13,37 @@ browser = playwright.chromium.launch(headless=False)
 page = browser.new_page()
 
 # 페이지 이동
-page.goto(url="https://google.com")
+page.goto(url="https://www.wanted.co.kr/")
 
-# 해당 화면 스크린샷
-page.screenshot(path="sc.png")
+time.sleep(2)
+
+page.click("button.Aside_searchButton__Xhqq3")
+
+time.sleep(2)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+
+time.sleep(2)
+
+page.keyboard.down("Enter")
+
+time.sleep(2)
+
+page.click("a#search_tab_position")
+
+time.sleep(2)
+
+for _ in range(5):
+    page.keyboard.down("End")
+    time.sleep(2)
+
+time.sleep(3)
+
+# html 가져오기
+content = page.content()
+
+# playwright 종료
+playwright.stop()
+
+
+soup = BeautifulSoup(content, "html.parser")
